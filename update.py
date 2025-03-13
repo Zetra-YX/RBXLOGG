7f994ed2b470a7801c5450f1bdf4706a23f06e3a
Zetra <94852849+Zetra-YX@users.noreply.github.com>
2025-03-13 20:28:12 -0300

    Update update.py

--- update.py
+++ update.py
@@ -1,3 +1,3 @@
 games = {
-    "Update": ("1.0.0", "https://drive.usercontent.google.com/download?id=1DX8UssYGJTZ7yfcj-HtBMuq8ByQUPBps&export=download&authuser=0&confirm=t&uuid=6c19f15c-aa99-43f4-aef9-e39cd5adeccc&at=AIrpjvPJl9rgpnNcjZ_4UUrHHDRu%3A1739510838774")
+    "Update": ("1.0.1", "https://drive.usercontent.google.com/download?id=15P97zbJDMGC6_nN6cArRClGVFzEmY9or&export=download&authuser=0&confirm=t&uuid=e6e89baa-bc36-40d8-a079-a2ad3a030ea3&at=AEz70l7Ccw0q8A2hX_jxffKO0pyh%3A1741908451474")
 }

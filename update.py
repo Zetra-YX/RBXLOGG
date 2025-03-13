6ccaaa9ee672bdfd74486ec87d8b445bc0e7f041
Zetra <94852849+Zetra-YX@users.noreply.github.com>
2025-03-13 20:15:52 -0300

    Update and rename usuarios.json to update.py

---
+++ update.py
@@ -0,0 +1,3 @@
+games = {
+    "Update": ("1.0.0", "https://drive.usercontent.google.com/download?id=1DX8UssYGJTZ7yfcj-HtBMuq8ByQUPBps&export=download&authuser=0&confirm=t&uuid=6c19f15c-aa99-43f4-aef9-e39cd5adeccc&at=AIrpjvPJl9rgpnNcjZ_4UUrHHDRu%3A1739510838774")
+}

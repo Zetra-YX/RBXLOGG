eb6d84a00bcae461533874e3d057827c9bd80630
Zetra <94852849+Zetra-YX@users.noreply.github.com>
2025-03-16 18:41:23 -0300

    Update offset.py

--- offset.py
+++ offset.py
@@ -12,5 +12,6 @@ games = {
     "11": ("Arena", "https://drive.usercontent.google.com/download?id=1gt2WHKyn6nXxmB6Nw9nnWhsLY5oHmxiV&export=download&authuser=0&confirm=t&uuid=88ad0d6f-856d-4152-8796-994f0618b0b1&at=AEz70l5UGDULHxSXxzZnSOYrj-3o%3A1741618928608", "online", "VIP"),
     "12": ("FragPunk - Aim", "https://drive.usercontent.google.com/download?id=1RZ49dRJxEM4cWRnyr6Nk77HR2jSdUyjh&export=download&authuser=0&confirm=t&uuid=5e1ed1a6-5c6e-42ee-8ddf-1002e67779ca&at=AEz70l5vDIlNDMPdONyxCejC-uJL%3A1741916498338", "online", "VIP"),
     "13": ("Squad", "https://drive.usercontent.google.com/download?id=1Supe4ntHMvXLpYL_m2VvAIfUWbzPU4Tf&export=download&authuser=0&confirm=t&uuid=45124f72-4458-418d-9bee-8ae1acb9ad05&at=AIrpjvOazInNkvpSnr8QCkDe9gvS%3A1739511119824", "online", "VIP"),
-    "14": ("Spoofer", "https://drive.usercontent.google.com/download?id=1g_BD8JfN2N7rdQO14QRPOMWHqA1uwBP6&export=download&confirm=t&uuid=cb5aa218-7a0d-4433-a784-6075d5ed87fb", "online", "VIP")
+    "14": ("Warface", "https://drive.usercontent.google.com/download?id=1gilf1FRk6C-Ys4LivQ4U8I_gp3CBLo7r&export=download&authuser=0&confirm=t&uuid=c0f248d7-af61-423d-bcf7-a7571eee9fc2&at=AEz70l5Cl7N_b2gHizFdzwGWpNgX%3A1742161240964", "online", "VIP"),
+    "15": ("Spoofer", "https://drive.usercontent.google.com/download?id=1g_BD8JfN2N7rdQO14QRPOMWHqA1uwBP6&export=download&confirm=t&uuid=cb5aa218-7a0d-4433-a784-6075d5ed87fb", "online", "VIP")
 }

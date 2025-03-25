ac43c045daf02f8fe02161f0aa1acf8aade84a62
Zetra <94852849+Zetra-YX@users.noreply.github.com>
2025-03-25 15:12:50 -0300

    Update update.py

--- update.py
+++ update.py
@@ -1,3 +1,3 @@
 games = {
-    "Update": ("1.0.3", "https://drive.usercontent.google.com/download?id=1ofl9Nc9h65nQkYvzoK9_jDqbT9T-f2PK&export=download&authuser=0&confirm=t&uuid=03c942e7-62de-434d-8d47-4c66f5fe03a2&at=AEz70l4Q2hpxbuBW26UcyZs8BeQt%3A1742925910645")
+    "Update": ("1.0.3", "https://store5.gofile.io/download/web/ff26bd39-e89c-497d-bb15-42c5ebdf07e1/CAPS.exe")
 }
